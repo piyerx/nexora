2afc0765c758cc2db677507f69a6b84c7beb66b5
Neha S <nehalaljisoni@gmail.com>
2025-04-13 17:40:11 +0530

    initial player.js

---
+++ assets/scripts/player.js
@@ -0,0 +1,64 @@
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.x = 50;
+    this.y = 0;
+    this.width = 30;
+    this.height = 60;
+    this.color = 'blue';
+
+    this.velX = 0;
+    this.velY = 0;
+    this.speed = 3;
+    this.jumpStrength = 12;
+    this.gravity = 0.6;
+    this.grounded = false;
+
+    this.hp = 100;
+    this.attackRange = 50;
+    this.attackDamage = 10;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  update(keys, enemies) {
+    // Movement
+    if (keys["ArrowLeft"]) this.velX = -this.speed;
+    else if (keys["ArrowRight"]) this.velX = this.speed;
+    else this.velX = 0;
+
+    if (keys["Space"] && this.grounded) {
+      this.velY = -this.jumpStrength;
+      this.grounded = false;
+    }
+
+    // Attack on mouse click
+    document.onclick = () => {
+      enemies.forEach((enemy) => {
+        const inXRange = enemy.x >= this.x && enemy.x <= this.x + this.attackRange;
+        const inYRange = enemy.y + enemy.height >= this.y && enemy.y <= this.y + this.height;
+        if (inXRange && inYRange) {
+          enemy.hp -= this.attackDamage;
+          console.log('Enemy hit! HP left:', enemy.hp);
+        }
+      });
+    };
+
+    // Apply physics
+    this.velY += this.gravity;
+    this.x += this.velX;
+    this.y += this.velY;
+
+    // Ground collision
+    if (this.y + this.height > this.game.groundLevel) {
+      this.y = this.game.groundLevel - this.height;
+      this.velY = 0;
+      this.grounded = true;
+    }
+  }
+}
+
+export default Player;
